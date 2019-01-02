9cf4d7f649e242be94fc3f432da248a065d06759
Kim Bradley <kim.jamie.bradley@gmail.com>
2019-01-02 11:50:30 +0000

    Move code to language file
    Signed-off-by: Kim Bradley <kim.jamie.bradley@gmail.com>

---
+++ wlinux-setup.d/language.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source common.sh
+
+if (whiptail --title "Language" --yesno "Would you like to configure default keyboard input/language?" 8 65) then
+    echo "Running $ dpkg-reconfigure locales"
+    sudo dpkg-reconfigure locales
+fi
