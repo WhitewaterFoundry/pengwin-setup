711b66794c5fd3916dd2254e211e911c80f372e7
Kim Bradley <kim.jamie.bradley@gmail.com>
2019-01-21 10:27:33 +0000

    Replace repeated 'echo _ | sudo tee -a _' with 'sudo tee -a _ << EOF _ EOF'
    Signed-off-by: Kim Bradley <kim.jamie.bradley@gmail.com>

--- wlinux-setup.d/shell-opts.sh
+++ wlinux-setup.d/shell-opts.sh
@@ -3,8 +3,6 @@
 source "/usr/local/wlinux-setup.d/common.sh"
 
 function InputRc {
-WLINUX_STRING="### WLINUX OPTIMISED DEFAULTS"
-
 echo "Installing optimised inputrc commands to /etc/inputrc"
 
 if [[ ! -f "/etc/inputrc" ]] ; then
@@ -13,7 +11,7 @@ fi
 
 cat /etc/inputrc | while read line
 do
-	if [[ $line == *WLINUX_STRING ]] ; then
+	if [[ $line == *"### WLINUX OPTIMISED DEFAULTS"* ]] ; then
 		echo "Previous wlinux inputrc customizations detected. Cancelling install..."
 		whiptail --title "Warning!" --msgbox "Previous install of wlinux inputrc customizations detected. To reinstall, please edit \"/etc/inputrc\" with your favourite text editor and remove all of the text between (and including) the lines $WLINUX_STRING" 15 95
 		return
@@ -21,43 +19,45 @@ do
 done
 
 # Write Carlos' custom inputrc to global inputrc (see: https://github.com/crramirez/shellprefs/blob/master/.inputrc)
-echo ${WLINUX_STRING}								| sudo tee -a /etc/inputrc
-echo "# Don't ring bell on completion" 				| sudo tee -a /etc/inputrc
-echo "set bell-style none" 							| sudo tee -a /etc/inputrc
-echo "" 											| sudo tee -a /etc/inputrc
-echo "# Filename completion/expansion" 				| sudo tee -a /etc/inputrc
-echo "set completion-ignore-case on" 				| sudo tee -a /etc/inputrc
-echo "set show-all-if-ambiguous on" 				| sudo tee -a /etc/inputrc
-echo "set show-all-if-unmodified on" 				| sudo tee -a /etc/inputrc
-echo "" 											| sudo tee -a /etc/inputrc
-echo "# Append "/" to all dirnames" 				| sudo tee -a /etc/inputrc
-echo "set mark-directories on" 						| sudo tee -a /etc/inputrc
-echo ""												| sudo tee -a /etc/inputrc
-echo "# Mark symlinked directories"					| sudo tee -a /etc/inputrc
-echo "set mark-symlinked-directories On"			| sudo tee -a /etc/inputrc
-echo ""												| sudo tee -a /etc/inputrc
-echo "# Do not match hidden files"					| sudo tee -a /etc/inputrc
-echo "set match-hidden-files off"					| sudo tee -a /etc/inputrc
-echo ""												| sudo tee -a /etc/inputrc
-echo "# Color files by type"						| sudo tee -a /etc/inputrc
-echo "set colored-stats On"							| sudo tee -a /etc/inputrc
-echo "# Append char to indicate type"				| sudo tee -a /etc/inputrc
-echo "set visible-stats On"							| sudo tee -a /etc/inputrc
-echo "# Color the common prefix"					| sudo tee -a /etc/inputrc
-echo "set colored-completion-prefix On"				| sudo tee -a /etc/inputrc
-echo "# Color the common prefix in menu-complete"	| sudo tee -a /etc/inputrc
-echo "set menu-complete-display-prefix On"			| sudo tee -a /etc/inputrc
-echo ""												| sudo tee -a /etc/inputrc
-echo "\"\e[A\": history-search-backward"			| sudo tee -a /etc/inputrc
-echo "\"\e[B\": history-search-forward"				| sudo tee -a /etc/inputrc
-echo ""												| sudo tee -a /etc/inputrc
-echo "$if Bash"										| sudo tee -a /etc/inputrc
-echo "  # 'Magic Space'"							| sudo tee -a /etc/inputrc
-echo "  # Insert a space character then performs"	| sudo tee -a /etc/inputrc
-echo "  # a history expansion in the line"			| sudo tee -a /etc/inputrc
-echo "  Space: magic-space"							| sudo tee -a /etc/inputrc
-echo "$endif"										| sudo tee -a /etc/inputrc
-echo ${WLINUX_STRING}								| sudo tee -a /etc/inputrc
+sudo tee -a /etc/inputrc << EOF
+### WLINUX OPTIMISED DEFAULTS
+# Don't ring bell on completion
+set bell-style none
+
+# Filename completion/expansion
+set completion-ignore-case on
+set show-all-if-ambiguous on
+set show-all-if-unmodified on
+
+# Append / to all dirnames
+set mark-directories on
+
+# Mark symlinked directories
+set mark-symlinked-directories On
+
+# Do not match hidden files
+set match-hidden-files off
+
+# Color files by type
+set colored-stats On
+# Append char to indicate type
+set visible-stats On
+# Color the common prefix
+set colored-completion-prefix On
+# Color the common prefix in menu-complete
+set menu-complete-display-prefix On
+
+\"\e[A\": history-search-backward
+\"\e[B\": history-search-forward
+
+\$if Bash
+  # Magic Space
+  # Insert a space character then performs
+  # a history expansion in the line
+  Space: magic-space
+\$endif
+### WLINUX OPTIMISED DEFAULTS
+EOF
 
 whiptail --title "Further customizations" --msgbox "To make further customizations you may either edit the global inputrc preferences under \"/etc/inputrc\", or for user-specific preferences edit \"~/.inputrc\" with the text editor of your choice." 16 95
 }
