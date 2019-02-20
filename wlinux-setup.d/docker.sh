fe628162a0688fa4732544674e7d43001888c751
Carlos Rafael Ramirez <crramirez@gmail.com>
2019-02-20 16:47:40 -0500

    Create the /mnt/c link code only if the user selected yes to move the root to /

--- wlinux-setup.d/docker.sh
+++ wlinux-setup.d/docker.sh
@@ -158,9 +158,7 @@ if (whiptail --title "DOCKER" --yesno "Would you like to install the bridge to D
                 sudo sed -i 's$\(root=\)\(.*\)$\1/$' /etc/wsl.conf
             fi
 
-        fi
-
-        cat << 'EOF' >> create-mnt-c-link
+            cat << 'EOF' >> create-mnt-c-link
 #!/bin/bash
 
 if [[ -z $(ls -A /mnt/c) ]]; then
@@ -169,16 +167,18 @@ if [[ -z $(ls -A /mnt/c) ]]; then
     ln -s $(wslpath -u "C:\\") /mnt/c
 fi
 EOF
-        sudo cp create-mnt-c-link /usr/bin/create-mnt-c-link
-        sudo chmod u+x /usr/bin/create-mnt-c-link
+            sudo cp create-mnt-c-link /usr/bin/create-mnt-c-link
+            sudo chmod u+x /usr/bin/create-mnt-c-link
 
-        echo '%sudo   ALL=NOPASSWD: /usr/bin/create-mnt-c-link' | sudo EDITOR='tee -a' visudo --quiet --file=/etc/sudoers.d/create-mnt-c-link
+            echo '%sudo   ALL=NOPASSWD: /usr/bin/create-mnt-c-link' | sudo EDITOR='tee -a' visudo --quiet --file=/etc/sudoers.d/create-mnt-c-link
 
-        cat << 'EOF' >> create-mnt-c-link.sh
+            cat << 'EOF' >> create-mnt-c-link.sh
 sudo create-mnt-c-link
 EOF
-        sudo cp create-mnt-c-link.sh /etc/profile.d/create-mnt-c-link.sh
-        sudo chmod -w /usr/bin/create-mnt-c-link
+            sudo cp create-mnt-c-link.sh /etc/profile.d/create-mnt-c-link.sh
+            sudo chmod -w /usr/bin/create-mnt-c-link
+        fi
+
 
     fi
 
