e3c5e67eb0437caa88990a80d6b553afdac34bfd
Carlos Ramirez <crramirez@gmail.com>
2019-02-21 20:17:20 -0500

    Cloud Management Tools - Added AWS CLI

--- wlinux-setup.d/cloudcli.sh
+++ wlinux-setup.d/cloudcli.sh
@@ -20,15 +20,51 @@ function install_terraform() {
   cleantmp
 }
 
+function install_awscli() {
+
+  if (whiptail --title "AWS CLI" --yesno "Would you like to install the AWS CLI Using the Bundled Installer?\n\nPython is required" 10 90) ; then
+    echo "Installing AWS CLI..."
+
+    if ! (python3 --version); then
+      bash ${SetupDir}/pythonpi.sh "$@"
+
+      if ! (python3 --version); then
+        return
+      fi
+    fi
+
+    createtmp
+
+    wget -O awscli-bundle.zip https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
+    unzip awscli-bundle.zip
+
+    sudo python3 awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+    echo "Installing bash-completion"
+    sudo mkdir -p /etc/bash_completion.d
+    sudo apt-get install -yq bash-completion
+
+    sudo cp /usr/local/aws/bin/aws_completer /usr/local/bin
+    sudo cp /usr/local/aws/bin/aws_bash_completer /etc/bash_completion.d/
+
+    aws --version
+
+    cleantmp
+  else
+    echo "Skipping AWS CLI"
+
+  fi
+}
+
 function install_and_set_cloud {
   EDITORCHOICE=$(
     whiptail --title "Cloud Management Menu" --checklist --separate-output "CLI tools for cloud management\n[SPACE to select, ENTER to confirm]:" 12 80 4 \
-      "TERRAFORM" "Terraform                   " off 3>&1 1>&2 2>&3
+      "TERRAFORM" "Terraform                   " off \
+      "AWS" "AWS CLI" off 3>&1 1>&2 2>&3
   )
-  
-  local exit_status=$?
-  
-  if [[ ${exit_status} != 0 ]] ; then
+
+  echo "Selected:" ${EDITORCHOICE}
+  if [[ ! ${EDITORCHOICE} ]] ; then
     return
   fi
   
@@ -41,6 +77,12 @@ function install_and_set_cloud {
     
   fi
   
+
+  if [[ ${EDITORCHOICE} == *"AWS"* ]] ; then
+
+    install_awscli
+
+  fi
 
 }
 
