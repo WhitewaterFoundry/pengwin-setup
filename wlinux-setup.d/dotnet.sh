3a175c9fb36b4d8d90b690a1c765197cdc6f09c9
Carlos Rafael Ramirez <crramirez@gmail.com>
2019-03-06 15:58:00 -0500

    Fix uninstallable package libssl1.0.2 in dotnet and vscode

--- wlinux-setup.d/dotnet.sh
+++ wlinux-setup.d/dotnet.sh
@@ -2,24 +2,27 @@
 
 source $(dirname "$0")/common.sh "$@"
 
-if (whiptail --title "DOTNET" --yesno "Would you like to download and install the .NET Core SDK for Linux?" 8 75) then
-    echo "Installing DOTNET"
-    createtmp
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-    sudo cp microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-    rm microsoft.gpg
-    sudo sh -c 'echo "deb https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/microsoft.list' 
-    updateupgrade
-    sudo apt install dotnet-sdk-2.1.300-rc1* -y
-    cleantmp
+if (whiptail --title "DOTNET" --yesno "Would you like to download and install the .NET Core SDK for Linux?" 8 75) ; then
+  echo "Installing DOTNET"
+  createtmp
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+  sudo cp microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 
-    if (whiptail --title "NUGET" --yesno "Would you like to download and install NuGet?" 8 50) then
-        echo "Installing NuGet"
-        sudo apt install nuget -y
-    else
-        echo "Skipping NUGET"
-    fi
+  echo 'deb https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main' | sudo tee /etc/apt/sources.list.d/microsoft.list
+  #Temporary: Fix issue with libssl1.0.2
+  echo 'deb https://deb.debian.org/debian stable main' | sudo tee /etc/apt/sources.list.d/stable.list
+
+  updateupgrade
+  sudo apt-get -y -q install dotnet-sdk-2.1
+  cleantmp
+
+  if (whiptail --title "NUGET" --yesno "Would you like to download and install NuGet?" 8 50) ; then
+    echo "Installing NuGet"
+    sudo apt-get -q -y install nuget
+  else
+    echo "Skipping NUGET"
+  fi
 
 else
-    echo "Skipping DOTNET"
+  echo "Skipping DOTNET"
 fi
