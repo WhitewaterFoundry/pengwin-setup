d565f93209fdcb644fc026a3ab4f1f233bd1d0f2
Kim (grufwub) <grufwub@gmail.com>
2019-03-08 19:40:17 +0000

    Corrects powershell script
    Signed-off-by: Kim (grufwub) <grufwub@gmail.com>

--- wlinux-setup.d/powershell.sh
+++ wlinux-setup.d/powershell.sh
@@ -2,14 +2,14 @@
 
 source $(dirname "$0")/common.sh "$@"
 
-if (whiptail --title "POWERSHELL" --yesno "Would you like to download and install Powershe$
+if (whiptail --title "POWERSHELL" --yesno "Would you like to download and install Powershell?" 8 55) then
     echo "Installing POWERSHELL"
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     sudo cp microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
     rm microsoft.gpg
-    sudo sh -c 'echo "deb https://packages.microsoft.com/repos/microsoft-debian-stretch-pr$
+    sudo sh -c 'echo "deb https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/microsoft.list'
     updateupgrade
-    sudo apt -t unstable install liblttng-ust0 libssl1.0.2 libicu57 liburcu6 liblttng-ust-$
+    sudo apt -t unstable install liblttng-ust0 libssl1.0.2 libicu57 liburcu6 liblttng-ust-ctl4
     sudo apt install powershell -y
 else
     echo "Skipping POWERSHELL"
