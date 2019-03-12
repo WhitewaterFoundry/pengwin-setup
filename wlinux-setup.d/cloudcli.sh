9064414f3df475bbf3c2652c1973cd8ae4378238
Carlos Rafael Ramirez <crramirez@gmail.com>
2019-03-11 19:22:01 -0500

    Add OpenStack, add confirm o terraform and a PoC about passing --yes to wlinux-setup

--- wlinux-setup.d/cloudcli.sh
+++ wlinux-setup.d/cloudcli.sh
@@ -3,26 +3,30 @@
 source $(dirname "$0")/common.sh "$@"
 
 function install_terraform() {
-  echo "Installing Terraform..."
+  if (confirm --title "OpenStack CLI" --yesno "Would you like to install the OpenStack command-line clients?\n\nPython 2.7+ is required" 10 90) ; then
+    echo "Installing Terraform..."
 
-  createtmp
+    createtmp
 
-  wget -O terraform.zip https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_$(dpkg --print-architecture).zip
-  unzip terraform.zip
-  sudo mv terraform /usr/bin
-  sudo chmod +x /usr/bin/terraform
+    wget -O terraform.zip https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_$(dpkg --print-architecture).zip
+    unzip terraform.zip
+    sudo mv terraform /usr/bin
+    sudo chmod +x /usr/bin/terraform
 
-  echo "Installing bash-completion"
-  sudo mkdir -p /etc/bash_completion.d
-  sudo apt-get install -yq bash-completion
+    echo "Installing bash-completion"
+    sudo mkdir -p /etc/bash_completion.d
+    sudo apt-get install -yq bash-completion
 
-  terraform -install-autocomplete
-  cleantmp
+    terraform -install-autocomplete
+    cleantmp
+  else
+    echo "Skipping Terraform"
+  fi
 }
 
 function install_awscli() {
 
-  if (whiptail --title "AWS CLI" --yesno "Would you like to install the AWS CLI Using the Bundled Installer?\n\nPython is required" 10 90) ; then
+  if (confirm --title "AWS CLI" --yesno "Would you like to install the AWS CLI Using the Bundled Installer?\n\nPython is required" 10 90) ; then
     echo "Installing AWS CLI..."
 
     if ! (python3 --version); then
@@ -56,31 +60,62 @@ function install_awscli() {
   fi
 }
 
-function install_and_set_cloud {
-  EDITORCHOICE=$(
-    whiptail --title "Cloud Management Menu" --checklist --separate-output "CLI tools for cloud management\n[SPACE to select, ENTER to confirm]:" 12 80 4 \
+function install_openstack() {
+
+  if (confirm --title "OpenStack CLI" --yesno "Would you like to install the OpenStack command-line clients?\n\nPython 2.7+ is required" 10 90) ; then
+    echo "Installing OpenStack CLI..."
+
+    sudo apt-get -y -q install python-dev python-pip
+    sudo pip install --upgrade setuptools
+    sudo pip install --upgrade python-openstackclient
+
+    echo "Installing bash-completion"
+    sudo mkdir -p /etc/bash_completion.d
+    sudo apt-get install -yq bash-completion
+
+    openstack complete | sudo tee /etc/bash_completion.d/osc.bash_completion > /dev/null
+
+    openstack --version
+
+  else
+    echo "Skipping OpenStack CLI"
+
+  fi
+
+}
+
+function main() {
+  local choice=$(
+    whiptail --title "Cloud Management Menu" --checklist --separate-output "CLI tools for cloud management\n[SPACE to select, ENTER to confirm]:" 12 60 4 \
       "TERRAFORM" "Terraform                   " off \
-      "AWS" "AWS CLI" off 3>&1 1>&2 2>&3
+      "AWS" "AWS CLI" off \
+      "OPENSTACK" "OpenStack command-line clients      " off 3>&1 1>&2 2>&3
   )
 
-  echo "Selected:" ${EDITORCHOICE}
-  if [[ ! ${EDITORCHOICE} ]] ; then
+  echo "Selected:" ${choice}
+  if [[ ! ${choice} ]] ; then
     return
   fi
   
-  if [[ ${EDITORCHOICE} == *"TERRAFORM"* ]] ; then
+  if [[ ${choice} == *"TERRAFORM"* ]] ; then
     
     install_terraform
     
   fi
   
 
-  if [[ ${EDITORCHOICE} == *"AWS"* ]] ; then
+  if [[ ${choice} == *"AWS"* ]] ; then
 
     install_awscli
 
   fi
 
+  if [[ ${choice} == *"OPENSTACK"* ]] ; then
+
+    install_openstack
+
+  fi
+
 }
 
-install_and_set_cloud
+main "$@"
