c0b16fbbe74e8c65a8c53a793770760b32bc4589
Carlos Rafael Ramirez <crramirez@gmail.com>
2019-03-28 21:47:33 -0500

    #363 [Pengwin-363] 1.2: add entry for home folder backup

--- pengwin-setup.d/home-backup.sh
+++ pengwin-setup.d/home-backup.sh
@@ -2,14 +2,28 @@
 
 source $(dirname "$0")/common.sh "$@"
 
+BACKUPS_DIR="${wHome}/Pengwin/backups"
+BACKUP_PATH="${BACKUPS_DIR}/pengwin_home.tgz"
+BACKUP_PATH_WIN="$(wslpath -w "${BACKUP_PATH}")"
+
 function backup() {
 
 
-  local dest_path="${wHomeWinPath}\\pengwin_home.tgz"
-  if (whiptail --title "BACKUP" --yesno "Would you like to backup your directory ${HOME} to ${dest_path} ?" 8 60) ; then
+  if (whiptail --title "BACKUP" --yesno "Would you like to backup your directory ${HOME} to ${BACKUP_PATH_WIN} ?" 8 60) ; then
     echo "Making a backup of ${HOME}"
 
-    tar -czvf "${wHome}/pengwin_home.tgz" ${HOME}
+    #Avoid to replace a previous backup
+    if [[ -f "${BACKUP_PATH}" ]]; then
+
+      local timestamp="$(date -r "${BACKUP_PATH}" '+%Y_%m_%d__%H_%M_%S')"
+
+      mv "${BACKUP_PATH}" "${BACKUPS_DIR}/pengwin_home_${timestamp}.tgz"
+    else
+
+      mkdir -p "${BACKUPS_DIR}"
+    fi
+
+    tar -czvf "${BACKUP_PATH}" ${HOME}
   else
     echo "Skipping BACKUP"
   fi
@@ -18,12 +32,10 @@ function backup() {
 
 function restore() {
 
+  if (whiptail --title "RESTORE" --yesno "Would you like to restore your directory ${HOME} from ${BACKUP_PATH_WIN} ?" 8 60) ; then
+    echo "Restoring from ${BACKUP_PATH_WIN}"
 
-  local src_path="${wHomeWinPath}\\pengwin_home.tgz"
-  if (whiptail --title "BACKUP" --yesno "Would you like to restore your directory ${HOME} from ${src_path} ?" 8 60) ; then
-    echo "Restoring from ${src_path}"
-
-    tar -xzvf "${wHome}/pengwin_home.tgz" --directory /
+    tar -xzvf "${BACKUP_PATH}" --directory /
   else
     echo "Skipping RESTORE"
   fi
@@ -31,17 +43,23 @@ function restore() {
 }
 
 function main {
-  local choice=$(
-    whiptail --title "Backup Menu" --radiolist --separate-output "Home folder Backup / Restore options\n[SPACE to select, ENTER to confirm]:" 10 80 4 \
-        "BACKUP" "Backup" off \
-        "RESTORE" "Restore                     " off 3>&1 1>&2 2>&3
-  )
+  local menu_choice=$(
 
-  if [[ ${choice} == "BACKUP" ]] ; then
+    menu --title "Backup Menu" --radiolist --separate-output "Home folder Backup / Restore options\n[SPACE to select, ENTER to confirm]:" 10 55 2 \
+        "BACKUP" 'Backups the ${HOME} directory   ' off \
+        "RESTORE" 'Restore the ${HOME} directory' off \
+
+    3>&1 1>&2 2>&3)
+
+  if [[ ${menu_choice} == "CANCELLED" ]] ; then
+    return 1
+  fi
+
+  if [[ ${menu_choice} == "BACKUP" ]] ; then
     backup
   fi
 
-  if [[ ${choice} == *"RESTORE"* ]] ; then
+  if [[ ${menu_choice} == "RESTORE" ]] ; then
     restore
   fi
 
