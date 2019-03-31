3199faa4bbd0ef6f363800493325f6b6a62ae3c8
Patrick Wu <wotingwu@live.com>
2019-03-31 16:00:59 +0800

    should work but icon didn't work

--- pengwin-setup.d/explorer.sh
+++ pengwin-setup.d/explorer.sh
@@ -5,7 +5,7 @@ source $(dirname "$0")/common.sh "$@"
 
 function install_explorer() {
 
-  local exec_name='wlinux.exe'
+  local exec_name='pengwin.exe'
   echo "WSL executable name: ${exec_name}"
   local plain_name='Pengwin'
 
@@ -14,21 +14,22 @@ function install_explorer() {
   if (confirm --title "EXPLORER" --yesno "Would you like to enable Windows Explorer shell integration?" 8 65); then
       echo "Enabling Windows Explorer shell integration."
       createtmp
-
+      mkdir $(wslpath "$(powershell.exe -NoProfile -NonInteractive -Command "Write-Output \$Env:USERPROFILE" | tr -d '\r')")/.pengwin
+      cp /usr/local/lib/pengwin.ico $(wslpath "$(powershell.exe -NoProfile -NonInteractive -Command "Write-Output \$Env:USERPROFILE" | tr -d '\r')")/.pengwin/pengwin.ico
       cat << EOF >> Install.reg
 Windows Registry Editor Version 5.00
 [HKEY_CURRENT_USER\Software\Classes\Directory\Background\shell\\Pengwin]
 @="Open with Pengwin"
-"Icon"="%USERPROFILE%\\AppData\\Local\\Packages\\WhitewaterFoundryLtd.Co.16571368D6CFF_kd1vv0z0vy70w\\LocalState\\rootfs\\usr\\local\\lib\\pengwin.ico"
+"Icon"="%USERPROFILE%\\.pengwin\\pengwin.ico"
 [HKEY_CURRENT_USER\Software\Classes\Directory\Background\shell\\Pengwin\command]
 @="_PengwinPath_ run \\"cd \\\\\\"\$(wslpath \\\\\\"%V\\\\\\")\\\\\\" && \$(getent passwd \$LOGNAME | cut -d: -f7)\\""
-"Icon"="%USERPROFILE%\\AppData\\Local\\Packages\\WhitewaterFoundryLtd.Co.16571368D6CFF_kd1vv0z0vy70w\\LocalState\\rootfs\\usr\\local\\lib\\pengwin.ico"
+"Icon"="%USERPROFILE%\\.pengwin\\pengwin.ico"
 [HKEY_CURRENT_USER\Software\Classes\Directory\shell\\Pengwin]
 @="Open with Pengwin"
-"Icon"="%USERPROFILE%\\AppData\\Local\\Packages\\WhitewaterFoundryLtd.Co.16571368D6CFF_kd1vv0z0vy70w\\LocalState\\rootfs\\usr\\local\\lib\\pengwin.ico"
+"Icon"="%USERPROFILE%\\.pengwin\\pengwin.ico"
 [HKEY_CURRENT_USER\Software\Classes\Directory\shell\\Pengwin\command]
 @="_PengwinPath_ run \\"cd \\\\\\"\$(wslpath \\\\\\"%V\\\\\\")\\\\\\" && \$(getent passwd \$LOGNAME | cut -d: -f7)\\""
-"Icon"="%USERPROFILE%\\AppData\\Local\\Packages\\WhitewaterFoundryLtd.Co.16571368D6CFF_kd1vv0z0vy70w\\LocalState\\rootfs\\usr\\local\\lib\\pengwin.ico"
+"Icon"="%USERPROFILE%\\.pengwin\\pengwin.ico"
 EOF
 
       local fullexec=$(wslpath -m "$(which ${exec_name})" | sed 's$/$\\\\\\\\$g')
@@ -64,7 +65,7 @@ Windows Registry Editor Version 5.00
 [-HKEY_CURRENT_USER\Software\Classes\Directory\Background\shell\\${plain_name}]
 [-HKEY_CURRENT_USER\Software\Classes\Directory\shell\\${plain_name}]
 EOF
-  cp Uninstall.reg $(wslpath "$(cmd.exe /c 'echo %TEMP%' 2>&1 | tr -d '\r')")/Uninstall.reg
+  cp Uninstall.reg $(wslpath "$(powershell.exe -NoProfile -NonInteractive -Command "Write-Output \$Env:TEMP" | tr -d '\r')")/Uninstall.reg
   cmd.exe /C "Reg import %TEMP%\Uninstall.reg"
 
   cleantmp
