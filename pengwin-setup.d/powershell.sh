ea7812754bc86142faa283140db6f1a693cf81b4
Hayden <hayden@whitewaterfoundry.com>
2019-04-12 16:04:05 -0400

    Fix powershell dependencies

--- pengwin-setup.d/powershell.sh
+++ pengwin-setup.d/powershell.sh
@@ -8,8 +8,12 @@ if (whiptail --title "POWERSHELL" --yesno "Would you like to download and instal
     sudo cp microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
     rm microsoft.gpg
     sudo sh -c 'echo "deb https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/microsoft.list'
+    sudo sh -c 'echo "deb https://deb.debian.org/debian/ stable main contrib non-free" > /etc/apt/sources.list.d/stable.list'
+    sudo sh -c 'echo " " >> /etc/apt/preferences'
+    sudo sh -c 'echo "Package: *" >> /etc/apt/preferences'
+    sudo sh -c 'echo "Pin: release a=stable" >> /etc/apt/preferences'
+    sudo sh -c 'echo "Pin-Priority: 499" >> /etc/apt/preferences'
     sudo apt-get update
-    sudo apt-get -t unstable install liblttng-ust0 libssl1.0.2 libicu57 liburcu6 liblttng-ust-ctl4
     sudo apt-get install powershell -y
 else
     echo "Skipping POWERSHELL"
