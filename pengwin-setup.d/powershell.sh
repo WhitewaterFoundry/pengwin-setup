714fb396b59bb2aece55727b2062afecfd45d983
Hayden <hayden@whitewaterfoundry.com>
2019-04-17 16:27:48 -0400

    Update powershell.sh

--- pengwin-setup.d/powershell.sh
+++ pengwin-setup.d/powershell.sh
@@ -9,10 +9,6 @@ if (whiptail --title "POWERSHELL" --yesno "Would you like to download and instal
     rm microsoft.gpg
     sudo sh -c 'echo "deb https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/microsoft.list'
     sudo sh -c 'echo "deb https://deb.debian.org/debian/ stable main contrib non-free" > /etc/apt/sources.list.d/stable.list'
-    sudo sh -c 'echo " " >> /etc/apt/preferences'
-    sudo sh -c 'echo "Package: *" >> /etc/apt/preferences'
-    sudo sh -c 'echo "Pin: release a=stable" >> /etc/apt/preferences'
-    sudo sh -c 'echo "Pin-Priority: 499" >> /etc/apt/preferences'
     sudo apt-get update
     sudo apt-get install powershell -y
 else
