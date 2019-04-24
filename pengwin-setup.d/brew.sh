f0987b45c5ca191cb6d7eb5881d15ccc6029319a
kim (grufwub) <grufwub@gmail.com>
2019-04-24 12:04:44 +0100

    Add brew package manager install script
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

---
+++ pengwin-setup.d/brew.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source $(dirname "$0")/common.sh "$@"
+
+if (whiptail --title "HOMEBREW" --yesno "Would you like to download and install the Homebrew package manager? Transitioning macOS users may find this more familiar, and others may use this to install both software not provided by APT, or newer versions of software not yet in APT repositories." 12 85) then
+	echo "Installing Homebrew"
+
+	# Check we have correct dependencies installed for brew
+	echo "Installing Homebrew dependencies"
+	sudo apt-get -y -q -t build-essential curl file git
+
+	whiptail --title "HOMEBREW" --msgbox "Please press enter when asked to continue by the Homebrew installer." 7 85
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/LinuxBrew/install/master/install.sh)"
+
+	echo "Adding Homebrew to system path"
+	sudo bash -c 'echo "eval \$($(brew --prefix)/bin/brew shellenv)" > /etc/profile/brew.sh'
+	source /etc/profile/brew.sh
+
+	whiptail --title "HOMEBREW" --msgbox "Please note, Homebrew does record and share analytics information (more information here: https://docs.brew.sh/Analytics.html). To opt-out, type:\n\`brew analytics off\`" 9 85
+else
+	echo "Skipping HOMEBREW"
+fi
