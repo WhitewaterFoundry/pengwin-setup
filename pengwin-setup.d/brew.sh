78f81ac563433723235f197d7f4adfd715e33df3
kim (grufwub) <grufwub@gmail.com>
2019-04-26 09:55:31 +0100

    Add warning message to brew install
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- pengwin-setup.d/brew.sh
+++ pengwin-setup.d/brew.sh
@@ -4,6 +4,8 @@ source $(dirname "$0")/common.sh "$@"
 
 if (whiptail --title "HOMEBREW" --yesno "Would you like to download and install the Homebrew package manager? Transitioning macOS users may find this more familiar, and others may use this to install both software not provided by APT, or newer versions of software not yet in APT repositories." 12 85) then
 	echo "Installing Homebrew"
+	whiptail --title "HOMEBREW" --msgbox "Please note, with Homebrew you can install many of the same packages at the same time as those offered by APT, or even offered by pengwin-setup. This is possible as Homebrew installs packages locally to:\n/home/linuxbrew\nTo allow forcing use of packages installed by a specific source, you may add an alias to them in:\n/etc/profile.d/99-alias-overrides.sh" 14 85
+	sudo bash -c ''
 
 	# Check we have correct dependencies installed for brew
 	echo "Installing Homebrew dependencies"
