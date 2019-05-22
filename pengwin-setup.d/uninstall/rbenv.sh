479b124b4a24e35b093c902f7e35cf86238bea4c
kim (grufwub) <grufwub@gmail.com>
2019-05-22 18:07:00 +0100

    Initial framework for rbenv uninstall script
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- pengwin-setup.d/uninstall/rbenv.sh
+++ pengwin-setup.d/uninstall/rbenv.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+source $(dirname "$0")/uninstall-common.sh
 
+function main()
+{
+
+echo "Uninstalling rbenv"
+
+echo "Removing $HOME/.rbenv"
+rm -rf "$HOME/.rbenv"
+
+echo "Removing PATH modifiers"
+
+
+}
+
+show_warning "rbenv" "$HOME/.rbenv" "$@"
+main "$@"
