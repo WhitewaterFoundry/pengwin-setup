9bfbb874b0e2b6749ce7a8c465076e7f7ffc3ebb
kim (grufwub) <grufwub@gmail.com>
2019-05-22 18:06:20 +0100

    Finish up 1st verion of uninstall-common script
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- pengwin-setup.d/uninstall/uninstall-common.sh
+++ pengwin-setup.d/uninstall/uninstall-common.sh
@@ -5,16 +5,20 @@ source $(dirname "$0")/../common.sh "$@"
 function show_warning()
 {
 
-# Usage: show_warning <INSTALL_ITEM> <INSTALL_DIR>
-if whiptail --title "!! $1 !!" --yesno "Are you sure you would like to uninstall $1?\n\nWhile you can reinstall $1 again from pengwin-setup, any of your own files in the '$2' install directory WILL BE PERMANENTLY DELETED.\n\nSelect 'yes' if you would like to proceed" 14 85 ; then
-	if whiptail --title "!! $1 !!" --yesno "Are you absolutely sure you'd like to proceed in uninstalling $1?" 7 85
-	
+# Usage: show_warning <UNINSTALL_ITEM> <UNINSTALL_DIR> <PREVIOUS_ARGS>
+local uninstall_item="$1"
+local uninstall_dir="$2"
+shift 2
+
+if whiptail --title "!! $uninstall_item !!" --yesno "Are you sure you would like to uninstall $uninstall_item?\n\nWhile you can reinstall $uninstall_item again from pengwin-setup, any of your own files in the '$uninstall_dir' install directory WILL BE PERMANENTLY DELETED.\n\nSelect 'yes' if you would like to proceed" 14 85 ; then
+	if whiptail --title "!! $uninstall_item !!" --yesno "Are you absolutely sure you'd like to proceed in uninstalling $uninstall_item?" 7 85 ; then
+		echo "User confirmed $uninstall_item uninstall"
+		return
 	fi
-else
-	echo "User cancelled"
-	
 fi
 
-}
+echo "$@"
+echo "User cancelled $1 uninstall"
+bash ${SetupDir}/uninstall.sh "$@"
 
-show_warning "rbenv" "$HOME/.rbenv"
+}
