fbb243778dd18c43a532a7b77cd087e3c2875574
kim (grufwub) <grufwub@gmail.com>
2019-05-31 09:42:24 +0100

    finish ibmcloud cli uninstall script
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- pengwin-setup.d/uninstall/ibmcli.sh
+++ pengwin-setup.d/uninstall/ibmcli.sh
@@ -2,15 +2,36 @@
 
 source $(dirname "$0")/uninstall-common.sh
 
+kubectl_key1='D0BC 747F D8CA F711 7500  D6FA 3746 C208 A731 7B0F'
+kubectl_key2='54A6 47F9 048D 5688 D7DA  2ABE 6A03 0B21 BA07 F4FB'
+
 function main()
 {
 
 echo "Uninstalling IBM Cloud CLI"
 
+sudo_rem_file "/usr/local/bin/bluemix"
+sudo_rem_file "/usr/local/bin/bx"
+sudo_rem_file "/usr/local/bin/ibmcloud"
+sudo_rem_file "/usr/local/bin/ibmcloud-analytics"
+sudo_rem_file "/usr/local/bin/helm"
+sudo_rem_file "/usr/local/bin/tiller"
 
+sudo_rem_dir "/usr/local/ibmcloud"
+rem_dir "$HOME/.bluemix"
+
+remove_package "kubectl"
+
+echo "Removing kubectl APT source..."
+sudo_rem_file "/etc/apt/sources.list.d/kubernetes.list"
+
+echo "Removing kubectl APT keys..."
+sudo apt-key del "$kubectl_key1"
+sudo apt-key del "$kubectl_key2"
 
 echo "Removing bash completions..."
 sudo_rem_file "/etc/bash_completion.d/ibmcli_completion"
+sudo_rem_file "/etc/bash_completion.d/helm_completions.bash"
 
 }
 
