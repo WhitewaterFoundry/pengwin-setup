453a04f7f41e90ffed1f89e98eb34514c46244a1
kim (grufwub) <grufwub@gmail.com>
2019-06-01 11:48:46 +0100

    finish theme uninstall script
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- pengwin-setup.d/uninstall/theme.sh
+++ pengwin-setup.d/uninstall/theme.sh
@@ -5,6 +5,15 @@ source $(dirname "$0")/uninstall-common.sh
 function main()
 {
 
+echo "Uninstalling Windows 10 themes"
+
+# Unset themes ?
+
+remove_package "lxappearance"
+
+sudo_rem_dir "/usr/share/themes/windows-10-dark"
+sudo_rem_dir "/usr/share/themes/windows-10-light"
+
 }
 
 if show_warning "" "" ; then
