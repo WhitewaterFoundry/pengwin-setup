227c3780a25d73f0591f94bf7e474cd7cc8209fc
kim (grufwub) <grufwub@gmail.com>
2019-06-02 12:56:02 +0100

    add powershell uninstall script
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

---
+++ pengwin-setup.d/uninstall/powershell.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source $(dirname "$0")/uninstall-common.sh
+
+function main()
+{
+
+echo "Uninstalling Powershell"
+
+remove_package "powershell"
+
+echo "Removing APT source(s)..."
+sudo_rem_file "/etc/apt/sources.list.d/microsoft.list"
+sudo_rem_file "/etc/apt/sources.list.d/stable.list"
+
+echo "Removing APT key..."
+sudo_rem_file "/etc/apt/trusted.gpg.d/microsoft.gpg"
+
+}
+
+if show_warning "" "" ; then
+	main "$@"
+fi
