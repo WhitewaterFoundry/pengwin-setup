86845f93a968991d465d0f9e3ff078ff568592a7
kim (grufwub) <grufwub@gmail.com>
2019-06-05 16:44:39 +0100

    finish sdkman java uninstall script
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- pengwin-setup.d/uninstall/java.sh
+++ pengwin-setup.d/uninstall/java.sh
@@ -5,6 +5,17 @@ sdkman_rgx1='^#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!'
 sdkman_rgx2='^[^#]*\bexport SDKMAN_DIR="$SDKMAN_DIR"'
 sdkman_rgx3='^[^#]*\b[[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"'
 
+function multiclean_file()
+{
+
+if [[ -f "$1" ]] ; then
+	clean_file "$1" "$sdkman_rgx1"
+	clean_file "$1" "$sdkman_rgx2"
+	clean_file "$1" "$sdkman_rgx3"
+fi
+
+}
+
 function main()
 {
 
@@ -12,10 +23,11 @@ echo "Uninstalling SDKMAN (Java)"
 
 rem_dir "$HOME/.sdkman"
 
-clean_file "$HOME/.bashrc"
-clean_file "$HOME/.zshrc"
-clean_file "$HOME/.profile"
-clean_file "$HOME/.bash_profile"
+echo "Removing PATH modifier(s)..."
+multiclean_file "$HOME/.bashrc"
+multiclean_file "$HOME/.zshrc"
+multiclean_file "$HOME/.profile"
+multiclean_file "$HOME/.bash_profile"
 
 echo "Removing bash completion..."
 sudo_rem_file "/etc/bash_completion.d/sdkman.bash"
