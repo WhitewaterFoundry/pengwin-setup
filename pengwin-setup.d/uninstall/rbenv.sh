8d6d9475c027abc6c9bd46a6c108e9b7163322d3
kim (grufwub) <grufwub@gmail.com>
2019-06-09 11:37:21 +0100

    update ruby uninstall to remove rails and offer nodejs uninstall
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- pengwin-setup.d/uninstall/rbenv.sh
+++ pengwin-setup.d/uninstall/rbenv.sh
@@ -14,6 +14,19 @@ rem_dir "$HOME/.rbenv"
 echo "Removing PATH modifier..."
 sudo_rem_file "/etc/profile.d/ruby.sh"
 
+# Check if Ruby on RAILS previously installed
+if [[ -d "$HOME/.gem"  ]] ; then
+	echo "Ruby on RAILS previously installed"
+	rm -rf "$HOME/.gem"
+
+	echo "Offering nodejs uninstall"
+	if (whiptail --title "rbenv" --yesno "It seems Ruby on RAILS was previously installed, for which nodejs would have been installed. Would you like to uninstall this too?" 8 85) ; then
+		bash $(dirname "$0")/nodejs.sh
+	else
+		echo "... user cancelled"
+	fi
+fi
+
 }
 
 if show_warning "rbenv" "$@" ; then
