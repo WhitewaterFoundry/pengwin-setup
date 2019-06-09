c697aca2f583f88bb9b26b2ea64953107b7a8466
kim (grufwub) <grufwub@gmail.com>
2019-06-09 12:31:19 +0100

    unset gtk settings on theme uninstall script
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- pengwin-setup.d/uninstall/theme.sh
+++ pengwin-setup.d/uninstall/theme.sh
@@ -7,8 +7,7 @@ function main()
 
 echo "Uninstalling Windows 10 themes"
 
-# Unset themes ?
-
+rem_file "$HOME/.gtkrc-2.0"
 sudo_rem_dir "/usr/share/themes/windows-10-dark"
 sudo_rem_dir "/usr/share/themes/windows-10-light"
 
