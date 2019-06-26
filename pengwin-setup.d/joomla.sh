6ca138713b39725e2f7b32799cc781ecf3f63b0c
Carlos Rafael Ramirez <crramirez@gmail.com>
2019-06-26 23:52:40 +0200

    #482 [Pengwin-482] Setup default database for Joomla and optimize php configuration

--- pengwin-setup.d/joomla.sh
+++ pengwin-setup.d/joomla.sh
@@ -12,11 +12,23 @@ function main() {
 
   bash ${SetupDir}/lamp.sh "$@" --yes
 
+  sudo service mysql start
+
+  sudo sed -i "s/\(output_buffering = \)\([0-9]*\)/\1Off/" /etc/php/7.*/apache2/php.ini
+  sudo service apache2 restart
+
   wget -O Joomla.tar.bz2 https://downloads.joomla.org/cms/joomla3/${JOOMLA_VERSION}/Joomla_${JOOMLA_VERSION}-Stable-Full_Package.tar.bz2?format=bz2
   sudo tar -xjvf Joomla.tar.bz2 --overwrite --directory /var/www/html/ #--strip-components 1 docker/docker
 
+  sudo chown -R www-data:www-data /var/www/html
   sudo rm -f /var/www/html/index.html
 
+  sudo mysql -u root << EOF
+
+CREATE DATABASE joomla;
+GRANT ALL PRIVILEGES ON joomla.* TO 'joomla'@'localhost' IDENTIFIED BY 'joomla';
+
+EOF
   wslview "http://localhost/index.php"
 
   cleantmp
