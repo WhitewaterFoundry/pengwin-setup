b6a8e2d8114b4a4e5b9352629df0efba80e03d95
Patrick Wu <wotingwu@live.com>
2019-06-28 00:49:14 +0800

    small fix in the menu

--- pengwin-setup.d/programming.sh
+++ pengwin-setup.d/programming.sh
@@ -6,12 +6,12 @@ function main() {
 
   local menu_choice=$(
 
-    menu --title "Programming Menu" --checklist --separate-output "Install various programming languages support\n[SPACE to select, ENTER to confirm]:" 16 95 8 \
+    menu --title "Programming Menu" --checklist --separate-output "Install various programming languages support\n[SPACE to select, ENTER to confirm]:" 16 95 9 \
       "C++" "Install support for Linux C/C++ programming in Visual Studio and CLion  " off \
       "DOTNET" "Install .NET Core SDK from Microsoft and optionally install NuGet  " off \
       "GO" "Install the latest Go from Google" off \
       "JAVA" "Install the SDKMan to manage Java SDKs" off \
-      "LATEX" "Install TexLive for Latex Support" \
+      "LATEX" "Install TexLive for LaTeX Support" off \
       "NODEJS" "Install Node.js and npm" off \
       "PYTHONPI" "Install Python 3.7 and download and install latest PyPi" off \
       "RUBY" "Install Ruby using rbenv and optionally install Rails" off \
