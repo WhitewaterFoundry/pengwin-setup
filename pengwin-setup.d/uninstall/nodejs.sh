3b1d1b94bbcb8a999361e330f1e00c97fed49e6d
kim (grufwub) <grufwub@gmail.com>
2019-07-01 16:35:54 +0100

    correct nodejs uninstall rails typo
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- pengwin-setup.d/uninstall/nodejs.sh
+++ pengwin-setup.d/uninstall/nodejs.sh
@@ -12,7 +12,7 @@ echo "Uninstalling nodejs (including n version manager, npm and yarn package man
 
 # Check for ruby rails install
 echo "Checking if Ruby on Rails installed..."
-if ! gem list --local | grep '^rails' > /dev/null 2>&1 ; then
+if gem list --local | grep '^rails' > /dev/null 2>&1 ; then
 	echo "Rails install detected. Showing user warning"
 	if ! (whiptail --title "nodejs" --yesno "A Ruby on Rails install has been detected, which relies upon nodejs. Are you sure you'd like to continue uninstalling nodejs? As this may break your Ruby on Rails install" 9 85) ; then
 		echo "User cancelled nodejs uninstall"
