aba29a63ac733b4284b590eca62cb011b1fb0ee9
kim (grufwub) <grufwub@gmail.com>
2019-07-01 12:15:02 +0100

    add start-menu entry cache regen to end of fcitx uninstall
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- pengwin-setup.d/uninstall/fcitx.sh
+++ pengwin-setup.d/uninstall/fcitx.sh
@@ -14,6 +14,9 @@ sudo_rem_file "/etc/profile.d/fcitx.sh"
 echo "Removing fcitx packages..."
 remove_package "fcitx" "fcitx-sunpinyin" "fcitx-libpinyin" "fcitx-rime" "fcitx-googlepinyin" "fcitx-chewing" "fcitx-mozc" "fcitx-kkc" "fcitx-kkc-dev" "fcitx-hangul" "fcitx-unikey" "fcitx-sayura" "fcitx-table" "fcitx-table-all"
 
+echo "Regenerating start-menu entry cache..."
+bash ${SetupDir}/shortcut.sh --yes
+
 }
 
 if show_warning "fcitx improved non-latin input" "$@" ; then
