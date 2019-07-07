252d5ae422d30f54425dab681612bb27d6cf595c
Carlos Rafael Ramirez <crramirez@gmail.com>
2019-07-07 17:46:11 +0200

    #482 [Pengwin-482] Download Joomla pack and expand into the default apache dir

--- pengwin-setup.d/joomla.sh
+++ pengwin-setup.d/joomla.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# shellcheck source=/usr/local/pengwin-setup.d/common.sh
+# shellcheck disable=SC1090
 source "$(dirname "$0")/common.sh" "$@"
 
 declare SetupDir
+declare wHome
 JOOMLA_VERSION="3-9-8"
 
 function main() {
@@ -18,18 +19,19 @@ function main() {
   sudo service apache2 restart
 
   wget -O Joomla.tar.bz2 https://downloads.joomla.org/cms/joomla3/${JOOMLA_VERSION}/Joomla_${JOOMLA_VERSION}-Stable-Full_Package.tar.bz2?format=bz2
-  sudo tar -xjvf Joomla.tar.bz2 --overwrite --directory /var/www/html/ #--strip-components 1 docker/docker
 
-  sudo chown -R www-data:www-data /var/www/html
-  sudo rm -f /var/www/html/index.html
+  local joomla_root="${wHome}/joomla_root"
+  mkdir -p "${joomla_root}"
+  sudo tar -xjvf Joomla.tar.bz2 --overwrite --directory "${joomla_root}"
 
+  sudo ln -s "${joomla_root}" /var/www/html/joomla_root
   sudo mysql -u root << EOF
 
 CREATE DATABASE joomla;
 GRANT ALL PRIVILEGES ON joomla.* TO 'joomla'@'localhost' IDENTIFIED BY 'joomla';
 
 EOF
-  wslview "http://localhost/index.php"
+  wslview "http://localhost/joomla_root/index.php"
 
   cleantmp
 }
