1545d5d78482609928e89c54a88697874961c069
Carlos Rafael Ramirez <crramirez@gmail.com>
2019-07-07 20:35:51 +0200

    #482 [Pengwin-482] Added Joomla to the menu and some code cleanup

--- pengwin-setup.d/programming.sh
+++ pengwin-setup.d/programming.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-source $(dirname "$0")/common.sh "$@"
+# shellcheck disable=SC1090
+source "$(dirname "$0")/common.sh" "$@"
+
+declare SetupDir
 
 function main() {
 
   local menu_choice=$(
 
-    menu --title "Programming Menu" --checklist --separate-output "Install various programming languages support\n[SPACE to select, ENTER to confirm]:" 16 95 8 \
+    menu --title "Programming Menu" --checklist --separate-output "Install various programming languages support\n[SPACE to select, ENTER to confirm]:" 16 95 9 \
       "C++" "Install support for Linux C/C++ programming in Visual Studio and CLion  " off \
       "DOTNET" "Install .NET Core SDK from Microsoft and optionally install NuGet  " off \
       "GO" "Install the latest Go from Google" off \
       "JAVA" "Install the SDKMan to manage Java SDKs" off \
+      "JOOMLA" "Install development support for Joomla" off \
       "NODEJS" "Install Node.js and npm" off \
       "PYTHONPI" "Install Python 3.7 and download and install latest PyPi" off \
       "RUBY" "Install Ruby using rbenv and optionally install Rails" off \
@@ -25,7 +29,6 @@ function main() {
   if [[ ${menu_choice} == *"C++"* ]] ; then
     echo "C++"
     bash ${SetupDir}/cpp-vs-clion.sh "$@"
-    exit_status=$?
   fi
 
   if [[ ${menu_choice} == *"DOTNET"* ]] ; then
@@ -41,6 +44,11 @@ function main() {
   if [[ ${menu_choice} == *"JAVA"* ]] ; then
     echo "JAVA"
     bash ${SetupDir}/java.sh "$@"
+  fi
+
+  if [[ ${menu_choice} == *"JOOMLA"* ]] ; then
+    echo "JOOMLA"
+    bash ${SetupDir}/joomla.sh "$@"
   fi
 
   if [[ ${menu_choice} == *"NODEJS"* ]] ; then
