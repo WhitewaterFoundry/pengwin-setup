36dee19005c9430cb29ac72f592f83048a012d51
Hayden <hayden@whitewaterfoundry.com>
2019-09-02 19:23:48 -0400

    Update nim.sh

--- pengwin-setup.d/nim.sh
+++ pengwin-setup.d/nim.sh
@@ -16,7 +16,7 @@ if (whiptail --title "NIM" --yesno "Would you like to download and install nim u
 
     echo "Saving environment variables to /etc/profile so they will persist."
     sudo sh -c 'echo "export NIMPATH=\${HOME}/.nimble/bin" >> /etc/profile.d/nim.sh'
-    sudo sh -c 'echo "export PATH=$NIMPATH:$PATH" >> /etc/profile.d/nim.sh'
+    sudo sh -c 'echo "export PATH=\$NIMPATH:\$PATH" >> /etc/profile.d/nim.sh'
 
     source /etc/profile.d/nim.sh
     cleantmp
