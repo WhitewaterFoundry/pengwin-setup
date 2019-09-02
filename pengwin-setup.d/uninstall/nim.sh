d1c4398d0e5709282ce69519325bce241cca60f6
Hayden <hayden@whitewaterfoundry.com>
2019-09-02 19:23:12 -0400

    Update nim.sh

--- pengwin-setup.d/uninstall/nim.sh
+++ pengwin-setup.d/uninstall/nim.sh
@@ -6,16 +6,8 @@ function main()
 {
 
 local nim_conf="/etc/profile.d/nim.sh"
-
-echo "Uninstalling nim"
-
-echo "Removing ~/.choosenim/"
 rem_dir "$HOME/.choosenim"
-
-echo "Removing ~/.nimble/"
 rem_dir "$HOME/.nimble"
-
-echo "Removing PATH modifier..."
 sudo_rem_file "/etc/profile.d/nim.sh"
 
 }
