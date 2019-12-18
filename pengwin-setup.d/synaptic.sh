a051234af51e136209958ff95e8578516cde0dfd
Carlos Rafael Ramirez <crramirez@gmail.com>
2019-12-18 22:17:29 +0100

    Add lsb-release to the synaptic installer

--- pengwin-setup.d/synaptic.sh
+++ pengwin-setup.d/synaptic.sh
@@ -2,7 +2,7 @@
 
 if (whiptail --title "SYNAPTIC" --yesno "Would you like to install the Synaptic package manager? This provides a graphical front-end for the APT package management system" 8 80) then
 	echo "Installing Synaptic"
-	sudo apt-get install -y -q synaptic
+	sudo apt-get install -y -q synaptic lsb-release
 else
 	echo "Skipping Synaptic"
 fi
