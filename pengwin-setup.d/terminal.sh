ac820affad65bba6e7b05e1b3dbd0966f59d8ad1
Patrick Wu <wotingwu@live.com>
2020-02-13 23:51:47 +0800

    structure completion

--- pengwin-setup.d/terminal.sh
+++ pengwin-setup.d/terminal.sh
@@ -7,9 +7,9 @@ function main() {
     menu --title "Terminal Menu" --checklist --separate-output "Install Terminals you want\n[SPACE to select, ENTER to confirm]:" 12 70 5 \
       "TILIX" "Tilix (requires X Server)" off \
       "WINTERMINAL" "Windows Terminal" off \
-      "DOCKER" "Install a secure bridge to Docker Desktop" off \
-      "HOMEBREW" "Install the Homebrew package manager" off \
-      "POWERSHELL" "Install PowerShell for Linux" off \
+      "WSLTTY" "WSLtty" off \
+      "GTERM" "Gnome Terminal (requires X Server)" off \
+      "XFTERM" "Xfce Terminal (requires X Server)" off \
 
   3>&1 1>&2 2>&3)
 
@@ -17,29 +17,23 @@ function main() {
     return 1
   fi
 
-  if [[ ${menu_choice} == *"ANSIBLE"* ]] ; then
-    echo "ANSIBLE"
-    bash ${SetupDir}/ansible.sh "$@"
+  if [[ ${menu_choice} == *"TILIX"* ]] ; then
+
   fi
 
-  if [[ ${menu_choice} == *"CLOUDCLI"* ]] ; then
-    echo "CLOUDCLI"
-    bash ${SetupDir}/cloudcli.sh "$@"
+  if [[ ${menu_choice} == *"WINTERMINAL"* ]] ; then
+
   fi
 
-  if [[ ${menu_choice} == *"DOCKER"* ]] ; then
-    echo "DOCKER"
-    bash ${SetupDir}/docker.sh "$@"
+  if [[ ${menu_choice} == *"WSLTTY"* ]] ; then
+
   fi
 
-  if [[ ${menu_choice} == *"HOMEBREW"* ]] ; then
-    echo "HOMEBREW"
-    bash ${SetupDir}/brew.sh "$@"
+  if [[ ${menu_choice} == *"GTERM"* ]] ; then
   fi
 
-  if [[ ${menu_choice} == *"POWERSHELL"* ]] ; then
-    echo "POWERSHELL"
-    bash ${SetupDir}/powershell.sh "$@"
+  if [[ ${menu_choice} == *"XFTERM"* ]] ; then
+
   fi
 
 }
