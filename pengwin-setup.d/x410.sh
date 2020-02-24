836dc57b07addd8bdd1c1a01bf5ae27b43aab67b
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2020-02-24 18:20:33 +0100

    Do not start x410 with public access in WSL1

--- pengwin-setup.d/x410.sh
+++ pengwin-setup.d/x410.sh
@@ -6,7 +6,12 @@ if [ -x "$(command -v x410.exe)" ]; then
   sudo bash -c 'cat > /etc/profile.d/02-x410.sh' << EOF
 #!/bin/bash
 
-(cmd-exe /c x410.exe /wm /public &> /dev/null &)
+if [ -n "${WSL2}" ]; then
+  (cmd-exe /c x410.exe /wm /public &> /dev/null &)
+else
+  (cmd-exe /c x410.exe /wm &> /dev/null &)
+fi
+
 EOF
 else
   if (whiptail --title "X410" --yesno "It seems that X410 is not installed on your machine. Would you like to view a link to X410 (recommended) on the Microsoft Store?" 8 80) then
