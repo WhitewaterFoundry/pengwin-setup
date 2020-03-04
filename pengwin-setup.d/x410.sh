93f55273eab44a66edece1b655b3e91b45766776
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2020-03-05 00:20:26 +0100

    Add a warning on x410 startup

--- pengwin-setup.d/x410.sh
+++ pengwin-setup.d/x410.sh
@@ -2,8 +2,9 @@
 
 # check if x410 exists
 if [ -x "$(command -v x410.exe)" ]; then
-  echo "Configuring X410 to start on Pengwin launch"
-  sudo bash -c 'cat > /etc/profile.d/02-x410.sh' << EOF
+  if (whiptail --title "X410" --yesno "It seems that X410 is already installed on your machine. Would you like to start it every time that Pengwin launchs?" 10 80) then
+    echo "Configuring X410 to start on Pengwin launch"
+    sudo bash -c 'cat > /etc/profile.d/02-x410.sh' << EOF
 #!/bin/bash
 
 if [ -n "${WSL2}" ]; then
@@ -13,8 +14,11 @@ else
 fi
 
 EOF
+  else
+    echo "Skipping X410"
+  fi
 else
-  if (whiptail --title "X410" --yesno "It seems that X410 is not installed on your machine. Would you like to view a link to X410 (recommended) on the Microsoft Store?" 8 80) then
+  if (whiptail --title "X410" --yesno "It seems that X410 is not installed on your machine. Would you like to view a link to X410 (recommended) on the Microsoft Store?" 10 80) then
     echo "Running $ wslview <link>"
     wslview https://afflnk.microsoft.com/c/1291904/433017/7593?u=https%3A%2F%2Fwww.microsoft.com%2Fen-us%2Fp%2Fx410%2F9nlp712zmn9q%23activetab%3Dpivot%3Aoverviewtab
   else
