069b6ba171070ef48523bebe5ca05c56d42fd713
Carlos Rafael Ramirez <crramirez@gmail.com>
2020-04-08 01:39:17 +0200

    Enlarge GUI menu, some code clean up

--- pengwin-setup.d/gui.sh
+++ pengwin-setup.d/gui.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-source $(dirname "$0")/common.sh "$@"
+# shellcheck source=/usr/local/pengwin-setup.d/common.sh
+source "$(dirname "$0")/common.sh" "$@"
 
 #Imported from common.h
 declare SetupDir
 
 function main() {
 
+  # shellcheck disable=SC2155
   local menu_choice=$(
 
-    menu --title "GUI Menu" --checklist --separate-output "Install an X server or various other GUI applications\n[SPACE to select, ENTER to confirm]:" 16 99 8 \
+    menu --title "GUI Menu" --checklist --separate-output "Install an X server or various other GUI applications\n[SPACE to select, ENTER to confirm]:" 17 99 9 \
       "NLI" "Install fcitx or iBus for improved non-Latin input support" off \
       "GUILIB" "Install a base set of libraries for GUI applications" off \
       "HIDPI" "Configure Qt and GTK for HiDPI displays (experimental)" off \
@@ -22,72 +24,74 @@ function main() {
 
   3>&1 1>&2 2>&3)
 
-  if [[ ${menu_choice} == "CANCELLED" ]] ; then
+  if [[ ${menu_choice} == "CANCELLED" ]]; then
     return 1
   fi
 
-  if [[ ${menu_choice} == *"X410"* ]] ; then
+  if [[ ${menu_choice} == *"X410"* ]]; then
     echo "X410"
-    bash ${SetupDir}/x410.sh "$@" 
+    bash "${SetupDir}"/x410.sh "$@"
   fi
 
-  if [[ ${menu_choice} == *"VCXSRV"* ]] ; then
+  if [[ ${menu_choice} == *"VCXSRV"* ]]; then
     echo "VCXSRV"
-    bash ${SetupDir}/vcxsrv.sh "$@"
+    bash "${SetupDir}"/vcxsrv.sh "$@"
   fi
 
-  if [[ ${menu_choice} == *"GUILIB"* ]] ; then
+  if [[ ${menu_choice} == *"GUILIB"* ]]; then
     echo "GUILIB"
-    bash ${SetupDir}/guilib.sh "$@"
+    bash "${SetupDir}"/guilib.sh "$@"
   fi
 
-  if [[ ${menu_choice} == *"NLI"* ]] ; then
+  if [[ ${menu_choice} == *"NLI"* ]]; then
     echo "NLI"
-      local nli_choice=$(
+    # shellcheck disable=SC2155
+    local nli_choice=$(
 
-        menu --title "Non-Latin Input" --radiolist --separate-output "Select your choice of input [SPACE to select, ENTER to confirm]:" 9 70 2 \
-          "FCITX" "Install fcitx for improved non-Latin input support" off \
-          "IBUS" "Install iBus for improved non-Latin input support" off \
+      menu --title "Non-Latin Input" --radiolist --separate-output "Select your choice of input [SPACE to select, ENTER to confirm]:" 9 70 2 \
+        "FCITX" "Install fcitx for improved non-Latin input support" off \
+        "IBUS" "Install iBus for improved non-Latin input support" off \
 
       3>&1 1>&2 2>&3)
-      if [[ ${nli_choice} == *"FCITX"* ]] ; then
-        echo "FCITX"
-        bash ${SetupDir}/fcitx.sh
-      fi
 
-      if [[ ${nli_choice} == *"IBUS"* ]] ; then
-        echo "IBUS"
-        bash ${SetupDir}/ibus.sh
-      fi
+    if [[ ${nli_choice} == *"FCITX"* ]]; then
+      echo "FCITX"
+      bash "${SetupDir}"/fcitx.sh
+    fi
 
-      if [[ ${nli_choice} == "CANCELLED" ]] ; then
-        echo "skip NLI"
-      fi
+    if [[ ${nli_choice} == *"IBUS"* ]]; then
+      echo "IBUS"
+      bash "${SetupDir}"/ibus.sh
+    fi
+
+    if [[ ${nli_choice} == "CANCELLED" ]]; then
+      echo "skip NLI"
+    fi
   fi
 
-  if [[ ${menu_choice} == *"HIDPI"* ]] ; then
+  if [[ ${menu_choice} == *"HIDPI"* ]]; then
     echo "HIDPI"
-    bash ${SetupDir}/hidpi.sh
+    bash "${SetupDir}"/hidpi.sh
   fi
 
-  if [[ ${menu_choice} == *"STARTMENU"* ]] ; then
+  if [[ ${menu_choice} == *"STARTMENU"* ]]; then
     echo "STARTMENU"
-    bash ${SetupDir}/shortcut.sh "$@"
+    bash "${SetupDir}"/shortcut.sh "$@"
   fi
 
-  if [[ ${menu_choice} == *"SYNAPTIC"* ]] ; then
+  if [[ ${menu_choice} == *"SYNAPTIC"* ]]; then
     echo "SYNAPTIC"
-    bash ${SetupDir}/synaptic.sh "$@"
+    bash "${SetupDir}"/synaptic.sh "$@"
   fi
 
-  if [[ ${menu_choice} == *"THEME"* ]] ; then
+  if [[ ${menu_choice} == *"THEME"* ]]; then
     echo "WINTHEME"
-    bash ${SetupDir}/theme.sh "$@"
+    bash "${SetupDir}"/theme.sh "$@"
   fi
 
-  if [[ ${menu_choice} == *"TERMINAL"* ]] ; then
+  if [[ ${menu_choice} == *"TERMINAL"* ]]; then
     echo "TERMINAL"
-    bash ${SetupDir}/terminal.sh "$@"
+    bash "${SetupDir}"/terminal.sh "$@"
   fi
 
 }
