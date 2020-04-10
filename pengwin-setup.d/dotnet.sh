131cd96f555ae0d46cf0e3b9137cdac6d4a04883
Carlos Rafael Ramirez <crramirez@gmail.com>
2020-04-10 15:10:25 +0200

    Upgrade dotnet to 3.1 LTS

--- pengwin-setup.d/dotnet.sh
+++ pengwin-setup.d/dotnet.sh
@@ -13,11 +13,9 @@ if (whiptail --title "DOTNET" --yesno "Would you like to download and install th
   sudo cp microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 
   echo 'deb https://packages.microsoft.com/repos/microsoft-debian-buster-prod buster main' | sudo tee /etc/apt/sources.list.d/microsoft.list
-  #Reverse temporary fix
-  sudo rm /etc/apt/sources.list.d/stable.list
   sudo apt-get update
 
-  sudo apt-get -y -q install dotnet-sdk-3.0
+  sudo debconf-apt-progress -- apt-get -y install dotnet-sdk-3.1
   cleantmp
 
   if (whiptail --title "NUGET" --yesno "Would you like to download and install NuGet?" 8 50) ; then
