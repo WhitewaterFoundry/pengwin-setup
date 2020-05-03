06a2b74299f14f00c3c5a56bfc89f2a7cee98af2
Carlos Rafael Ramirez <crramirez@gmail.com>
2020-05-03 23:30:38 +0200

    Update Azure CLI installer. Updated MariaDB and code clean up.

--- pengwin-setup.d/powershell.sh
+++ pengwin-setup.d/powershell.sh
@@ -3,18 +3,18 @@
 # shellcheck source=/usr/local/pengwin-setup.d/common.sh
 source "$(dirname "$0")/common.sh" "$@"
 
-#Imported from common.h
-declare SetupDir
+if (confirm --title "POWERSHELL" --yesno "Would you like to download and install Powershell?" 8 55); then
+  echo "Installing POWERSHELL"
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
+  sudo cp microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+  rm microsoft.gpg
+  sudo sh -c 'echo "deb https://packages.microsoft.com/repos/microsoft-debian-buster-prod buster main" > /etc/apt/sources.list.d/microsoft.list'
 
-if (whiptail --title "POWERSHELL" --yesno "Would you like to download and install Powershell?" 8 55) then
-    echo "Installing POWERSHELL"
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-    sudo cp microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-    rm microsoft.gpg
-    sudo sh -c 'echo "deb https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/microsoft.list'
-    sudo sh -c 'echo "deb https://deb.debian.org/debian/ stretch main contrib non-free" > /etc/apt/sources.list.d/stretch.list'
-    sudo apt-get update
-    sudo apt-get install powershell -y
+  #remove the old stretch repository
+  sudo rm -f /etc/apt/sources.list.d/stretch.list
+
+  sudo apt-get update
+  sudo debconf-apt-progress -- apt-get install -y powershell
 else
-    echo "Skipping POWERSHELL"
+  echo "Skipping POWERSHELL"
 fi
