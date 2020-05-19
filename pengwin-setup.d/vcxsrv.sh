f2fbe5719cf1c06cee979a0538c705272a527a21
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2020-05-19 11:46:55 +0200

    Upgrade VcXsrv to 1.20.8.1

--- pengwin-setup.d/vcxsrv.sh
+++ pengwin-setup.d/vcxsrv.sh
@@ -5,7 +5,7 @@ source "$(dirname "$0")/common.sh" "$@"
 
 if (confirm --title "VCXSRV" --yesno "Would you like to install the VcXsrv X-server? This will be installed to your Windows home directory under .vcxsrv" 8 80); then
   echo "Installing VcXsrv"
-  VcxsrvUrl="https://sourceforge.net/projects/vcxsrv/files/vcxsrv/1.20.6.0/vcxsrv-64.1.20.6.0.installer.exe/download"
+  VcxsrvUrl="https://sourceforge.net/projects/vcxsrv/files/vcxsrv/1.20.8.1/vcxsrv-64.1.20.8.1.installer.exe/download"
 
   echo "Installing required install dependencies"
   sudo debconf-apt-progress -- apt-get -y install wget unzip p7zip-full mesa-utils
