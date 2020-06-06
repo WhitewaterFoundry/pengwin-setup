061c0e2f725032323a12a3ad125fbc3174523b45
Carlos Rafael Ramirez <crramirez@gmail.com>
2020-06-06 23:05:17 +0200

    Improve HighDPI

--- tests/hidpi.sh
+++ tests/hidpi.sh
@@ -4,11 +4,10 @@ source commons.sh
 
 function testDPI() {
   scale_factor=$(wslsys -S -s)
-	scale_factor_int=$(IFS='.' read -r -a splitted <<< "${scale_factor}"; echo -n "${splitted[0]}")
 
   ../pengwin-setup --noupdate --assume-yes --noninteractive GUI HIDPI
   assertEquals QT_SCALE_FACTOR "1" "$(grep -c "QT_SCALE_FACTOR=${scale_factor}" /etc/profile.d/hidpi.sh)"
-  assertEquals GDK_SCALE "1" "$(grep -c "GDK_SCALE=${scale_factor_int}" /etc/profile.d/hidpi.sh)"
+  assertEquals GDK_SCALE "1" "$(grep -c "GDK_SCALE" /etc/profile.d/hidpi.sh)"
 }
 
 function testUninstall() {
