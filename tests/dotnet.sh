27e611935906ea7e61167ee1d3ac1120f54e178f
Carlos Ramirez <crramirez@gmail.com>
2020-08-07 00:50:48 +0200

    Fix dotnet and powershell installations

---
+++ tests/dotnet.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source commons.sh
+
+function testMain() {
+
+  ../pengwin-setup --noupdate --assume-yes --noninteractive PROGRAMMING DOTNET
+
+  for i in 'dotnet-sdk-3.1' 'nuget'; do
+    package_installed $i
+    assertTrue "package $i is not installed" "$?"
+  done
+
+  assertEquals ".NET Core was not installed" "1" "$(/usr/bin/dotnet --version | grep -c '3.1')"
+
+  command -v /usr/bin/nuget
+  assertEquals "NUGet was not installed" "0" "$?"
+}
+
+function testUninstall() {
+
+  ../pengwin-setup --noupdate --assume-yes --noninteractive UNINSTALL DOTNET
+
+  for i in 'dotnet-sdk-3.1' 'nuget'; do
+    package_installed $i
+    assertFalse "package $i is not uninstalled" "$?"
+  done
+
+  command -v /usr/bin/dotnet
+  assertEquals ".NET Core was not uninstalled" "1" "$?"
+
+  command -v /usr/bin/nuget
+  assertEquals "NUGet was not uninstalled" "1" "$?"
+}
+
+source shunit2
