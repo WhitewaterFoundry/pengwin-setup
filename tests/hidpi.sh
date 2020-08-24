3cf65cd20a879785c9d5592e404caec08c0a7d5d
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2020-08-24 18:59:26 +0200

    Update hidpi.sh

--- tests/hidpi.sh
+++ tests/hidpi.sh
@@ -5,13 +5,13 @@ source commons.sh
 function testDPI() {
   scale_factor=$(wslsys -S -s)
 
-  run_pengwinsetup autoinstall GUI HIDPI --debug
+  run_pengwinsetup autoinstall GUI HIDPI
   assertEquals QT_SCALE_FACTOR "1" "$(grep -c "QT_SCALE_FACTOR=${scale_factor}" /etc/profile.d/hidpi.sh)"
 }
 
 function testUninstall() {
 
-  ../pengwin-setup --noupdate --assume-yes --noninteractive UNINSTALL HIDPI > /dev/null 2>&1
+  run_pengwinsetup autoinstall UNINSTALL HIDPI
 
   test -f /etc/profile.d/hidpi.sh
   assertFalse "FILE HIDPI" "$?"
