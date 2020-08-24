679c33119072d8c299cbf629e1107ea6397d1881
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2020-08-24 10:49:21 +0200

    Create x410.sh

---
+++ tests/x410.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source commons.sh
+
+function testX410() {
+  run_pengwinsetup autoinstall GUI X410
+
+  assertTrue "FILE PROFILE-X410" "[ -f /etc/profile.d/02-x410.sh ]"
+}
+
+function testUninstall() {
+
+  run_pengwinsetup autoinstall UNINSTALL X410
+
+  assertFalse "FILE PROFILE-X410" "[ -f /etc/profile.d/02-x410.sh ]"
+}
+
+source shunit2
