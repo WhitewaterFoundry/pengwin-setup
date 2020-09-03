deab9184f2a938e11678d34f04c8989b6fbfe8f9
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2020-09-03 23:56:42 +0200

    Update mocks.sh

--- tests/mocks.sh
+++ tests/mocks.sh
@@ -10,6 +10,9 @@ function register_call() {
 function verify_call() {
   if [[ "${SHUNIT_TMPDIR}" ]]; then
     sync
+    
+    echo grep -F -c "$@" "${SHUNIT_TMPDIR}/${REGISTER_FILE}" 
+    cat "${SHUNIT_TMPDIR}/${REGISTER_FILE}"
     grep -F -c "$@" "${SHUNIT_TMPDIR}/${REGISTER_FILE}" >/dev/null
     return $?
   fi
