92d81e124308cb23117cd925df7e27556364ea0a
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2020-09-03 23:55:06 +0200

    Update x410.sh

--- tests/x410.sh
+++ tests/x410.sh
@@ -12,7 +12,7 @@ function testX410() {
   verify_call "cmd.exe /c x410.exe /wm"
   assertTrue "X410 WSL1" "$?"
 
-  WSL2=1 bash -x /etc/profile.d/02-x410.sh
+  WSL2=1 bash /etc/profile.d/02-x410.sh
   verify_call "cmd.exe /c x410.exe /wm /public"
   assertTrue "X410 WSL2" "$?"
 }
