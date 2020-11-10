254bb62ad5f41e1b95bb692b32325ac7d26c75f6
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2020-11-11 00:27:40 +0100

    Update go.sh

--- tests/go.sh
+++ tests/go.sh
@@ -14,19 +14,12 @@ function testGo() {
   command -v go
   assertEquals "GO was not installed" "0" "$?"
 
-  assertEquals "GO was not installed" "1" "$(run_command_as_testuser go version | grep -c '1.15.2')"
+  assertEquals "GO was not installed" "1" "$(go version | grep -c '1.15.2')"
   assertTrue "FILE DEFAULT-STRUCT-GO" "[ -d /home/${TEST_USER}/go ]"
   assertTrue "FILE DEFAULT-STRUCT-GO" "[ -d /home/${TEST_USER}/go/pkg ]"
   assertTrue "FILE DEFAULT-STRUCT-GO" "[ -d /home/${TEST_USER}/go/src ]"
   assertTrue "FILE DEFAULT-STRUCT-GO" "[ -d /home/${TEST_USER}/go/bin ]"
 
-  #WSL2= bash /etc/profile.d/02-x410.sh
-  #verify_call "cmd.exe /c x410.exe /wm"
-  #assertTrue "X410 WSL1" "$?"
-
-  #WSL2=1 bash /etc/profile.d/02-x410.sh
-  #verify_call "cmd.exe /c x410.exe /wm /public"
-  #assertTrue "X410 WSL2" "$?"
 }
 
 function testUninstall() {
