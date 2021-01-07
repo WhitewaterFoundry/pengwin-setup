877e6c5056bca5245ac581961c765f3165ec7318
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2021-01-07 20:57:01 +0100

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,14 +6,28 @@ set -e
 
 run_test ./update_pengwin.sh
 
-run_test ./pythonpi.sh
-run_test ./go.sh
-run_test ./ansible.sh
-run_test ./fish.sh
-run_test ./x410.sh
-run_test ./rclocal.sh
-run_test ./cpp-vs-clion_test.sh
-run_test ./hidpi.sh
-run_test ./dotnet.sh
-run_test ./brew.sh
-
+if [ -z ${CIRCLE_NODE_TOTAL} ]; then 
+  run_test ./pythonpi.sh
+  run_test ./go.sh
+  run_test ./ansible.sh
+  run_test ./fish.sh
+  run_test ./x410.sh
+  run_test ./rclocal.sh
+  run_test ./cpp-vs-clion_test.sh
+  run_test ./hidpi.sh
+  run_test ./dotnet.sh
+  run_test ./brew.sh
+elif [[ ${CIRCLE_NODE_INDEX} == 0 ]]; then
+  run_test ./pythonpi.sh
+elif [[ ${CIRCLE_NODE_INDEX} == 1 ]]; then
+  run_test ./go.sh
+  run_test ./ansible.sh
+  run_test ./fish.sh
+  run_test ./x410.sh
+  run_test ./rclocal.sh
+  run_test ./cpp-vs-clion_test.sh
+elif [[ ${CIRCLE_NODE_INDEX} == 2 ]]; then
+  run_test ./hidpi.sh
+  run_test ./dotnet.sh
+  run_test ./brew.sh
+fi
