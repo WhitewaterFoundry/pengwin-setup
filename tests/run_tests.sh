9c6a0444efcb026aa942349383a58c8689108d9a
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2021-02-10 11:58:22 +0100

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -28,6 +28,7 @@ elif [[ ${CIRCLE_NODE_INDEX} == 1 ]]; then
   run_test ./rclocal.sh
   run_test ./cpp-vs-clion_test.sh
   run_test ./hidpi.sh
+  run_test ./ansible.sh
 elif [[ ${CIRCLE_NODE_INDEX} == 2 ]]; then
   run_test ./brew.sh
   run_test ./jetbrains-support.sh
@@ -35,5 +36,4 @@ elif [[ ${CIRCLE_NODE_INDEX} == 3 ]]; then
   run_test ./dotnet.sh
   run_test ./guilib.sh
   run_test ./go.sh
-  run_test ./ansible.sh
 fi
