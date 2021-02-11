148fc82fbdeedfbcc795078d3c873d614971ab63
Carlos Ramirez <crramirez@gmail.com>
2021-02-11 16:23:52 +0100

    Fix Java Test

--- tests/java.sh
+++ tests/java.sh
@@ -3,13 +3,14 @@
 source commons.sh
 
 function testMain() {
-
+set -x
   run_pengwinsetup install PROGRAMMING JAVA
 
   check_script '/etc/profile.d/sdkman.sh'
 
   chmod 777 -R "${HOME}/.sdkman"
   assertEquals "SDKMan was not installed" "1" "$(run_command_as_testuser sdk version | grep -c 'SDKMAN 5')"
+  set +x
 }
 
 function testUninstall() {
