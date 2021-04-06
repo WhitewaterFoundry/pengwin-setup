6365488642fd650f0d99a642710b2142b8a79659
Carlos Ramirez <crramirez@gmail.com>
2021-04-07 00:10:06 +0200

    Fix a problem with fish and the dbus script

--- pengwin-setup.d/guilib.sh
+++ pengwin-setup.d/guilib.sh
@@ -50,8 +50,8 @@ EOF
 if which cmd.exe >/dev/null
 
   for line in (dbus-launch | string match '*=*')
-    set -l kv (string split -m 1 = -- $line )
-    set -gx $kv[1] (string trim -c '\'"' -- $kv[2])
+    set -l kv (string split -m 1 = -- \$line )
+    set -gx \$kv[1] (string trim -c '\\'"' -- \$kv[2])
   end
 end
 
