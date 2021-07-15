e6168bf12dcf42807ecb52fae714b2f759a015fc
Carlos Ramirez <crramirez@gmail.com>
2021-07-15 20:47:31 +0200

    Update docker version

--- pengwin-setup.d/docker.sh
+++ pengwin-setup.d/docker.sh
@@ -3,8 +3,8 @@
 # shellcheck source=common.sh
 source "$(dirname "$0")/common.sh" "$@"
 
-DOCKER_VERSION="19.03.8"
-DOCKER_COMPOSE_VERSION="1.25.4"
+DOCKER_VERSION="20.10.0"
+DOCKER_COMPOSE_VERSION="1.27.4"
 
 # Imported from common.sh
 declare wHome
