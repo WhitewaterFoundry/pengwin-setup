6dd89aee8eb2cf1694e2a4a3c3188e4710040114
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2021-12-31 15:20:13 +0100

    Fix link to download X410

--- pengwin-setup.d/x410.sh
+++ pengwin-setup.d/x410.sh
@@ -33,7 +33,7 @@ EOF
 else
   if (confirm --title "X410" --yesno "It seems that X410 is not installed on your machine. Would you like to view a link to X410 (recommended) on the Microsoft Store?" 10 80) then
     echo "Running $ wslview <link>"
-    wslview https://afflnk.microsoft.com/c/1291904/433017/7593?u=https%3A%2F%2Fwww.microsoft.com%2Fen-us%2Fp%2Fx410%2F9nlp712zmn9q%23activetab%3Dpivot%3Aoverviewtab
+    wslview 'ms-windows-store://pdp/?PRODUCTID=9nlp712zmn9q&cid=pengwin-setup'
   else
     echo "Skipping X410"
   fi
