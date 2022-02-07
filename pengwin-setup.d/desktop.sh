00ba1cde1de7bc20cc0754c74fb368fe0a367e42
Javier Santos <javier.david.santos@gmail.com>
2022-02-06 21:30:50 -0500

    [PEN-160] Add support to remote desktop
    Add the configuration to connect Desktop env with remote desktop

--- pengwin-setup.d/desktop.sh
+++ pengwin-setup.d/desktop.sh
@@ -53,13 +53,52 @@ function install_dependencies() {
   return $dependencies_instaled
 }
 
+function install_xrdp() {
+  install_packages xrdp
+  install_packages xorgxrdp
+  sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
+  sudo /etc/init.d/xrdp start
+  
+  sudo bash -c 'cat > /usr/local/bin/remote_desktop.sh' << EOF
+#!/bin/bash
+
+function execute_remote_desktop(){
+    host_ip=\$(ip -o -f inet addr show | grep -v 127.0.0 | awk '{printf "%s", \$4}' | cut -f1 -d/)
+    user_name=\$(whoami)
+    echo "username:s:\$user_name" > /tmp/remote_desktop_config.rdp
+    cd /tmp
+    mstsc.exe remote_desktop_config.rdp  /v:\$host_ip:3390 /f
+}
+
+execute_remote_desktop
+EOF
+
+    sudo bash -c 'cat > /usr/local/bin/start-xrdp' << EOF
+#!/bin/bash
+
+sudo service xrdp start >/dev/null 2>&1 
+EOF
+
+    sudo tee '/etc/profile.d/start-xrdp.sh' << EOF
+#!/bin/sh
+
+sudo /usr/local/bin/start-xrdp
+EOF
+
+  sudo chmod +x /usr/local/bin/start-xrdp
+  sudo chmod +x /usr/local/bin/remote_desktop.sh
+  echo '%sudo   ALL=NOPASSWD: /usr/local/bin/start-xrdp' | sudo EDITOR='tee ' visudo --quiet --file=/etc/sudoers.d/start-xrdp
+
+}
+xrdp
+
 function install_xfce() {
   if install_dependencies "$@" ; then
     install_packages xfce4-terminal
     install_packages xfce4
-
+    install_xrdp
     if package_installed "xfce4-terminal" && package_installed "xfce4"; then
-      create_shortcut "Xfce desktop (WSL)" "xfce4-session" "/usr/share/pixmaps/xfce4_xicon.png"
+      create_shortcut "Xfce desktop (WSL)" "/usr/local/bin/remote_desktop.sh" "/usr/share/pixmaps/xfce4_xicon.png"
     else
       echo "There is a problem with xfce4 instalation"
     fi
@@ -67,25 +106,11 @@ function install_xfce() {
 
 }
 
-function install_mate() {
-  if install_dependencies "$@" ; then
-    install_packages mate-desktop-environment
-
-    if package_installed "mate-desktop-environment" ; then
-      create_shortcut "Mate desktop (WSL)" "mate-session" "/usr/share/icons/mate/16x16/apps/mate-desktop.png"
-    else
-      echo "There is a problem with mate instalation"
-    fi
-  fi
-}
-
-
 function main() {
   local menu_choice=$(
 
     menu --title "GUI Menu" --checklist --separate-output "Install an X server or various other GUI applications\n[SPACE to select, ENTER to confirm]:" 8 50 2 \
       "XFCE" "Install XFCE Desktop environment" off \
-      "MATE" "Install MATE Desktop environment" off \
 
 
   3>&1 1>&2 2>&3)
@@ -96,10 +121,6 @@ function main() {
 
   if [[ ${menu_choice} == *"XFCE"* ]]; then
     install_xfce "$@"
-  fi
-
-  if [[ ${menu_choice} == *"MATE"* ]]; then
-    install_mate "$@"
   fi
 
 }
