34f7555d1e2ddf3a8c644ed35bda7a3e09317cb9
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2023-10-10 15:21:30 +0200

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -32,7 +32,6 @@ elif [[ ${CIRCLE_NODE_INDEX} == $((i++)) ]]; then #1
   run_test ./desktop.sh
   run_test ./lamp.sh "BUILTIN"
 elif [[ ${CIRCLE_NODE_INDEX} == $((i++)) ]]; then #2
-  run_test ./fish.sh
   run_test ./x410.sh
   run_test ./rclocal.sh
   run_test ./cpp-vs-clion_test.sh
@@ -40,7 +39,6 @@ elif [[ ${CIRCLE_NODE_INDEX} == $((i++)) ]]; then #2
   run_test ./ansible.sh
 elif [[ ${CIRCLE_NODE_INDEX} == $((i++)) ]]; then #3
   run_test ./terraform.sh
-  run_test ./brew.sh
   run_test ./nodejs_lts.sh
 elif [[ ${CIRCLE_NODE_INDEX} == $((i++)) ]]; then #4
   run_test ./nodejs_n.sh
@@ -51,6 +49,7 @@ elif [[ ${CIRCLE_NODE_INDEX} == $((i++)) ]]; then #5
   run_test ./jetbrains-support.sh
 elif [[ ${CIRCLE_NODE_INDEX} == $((i++)) ]]; then #6
   #run_test ./lamp.sh "10.9"
+  run_test ./brew.sh
   run_test ./nodejs_latest.sh
 elif [[ ${CIRCLE_NODE_INDEX} == $((i++)) ]]; then #7
   run_test ./pythonpi.sh
@@ -58,4 +57,5 @@ elif [[ ${CIRCLE_NODE_INDEX} == $((i++)) ]]; then #8
   run_test ./java.sh
 elif [[ ${CIRCLE_NODE_INDEX} == $((i++)) ]]; then #9
   run_test ./go.sh
+  run_test ./fish.sh
 fi
