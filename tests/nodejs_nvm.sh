aae2b6817051b6c6d55389fbcfeac090a2b2cf99
Carlos Ramirez <crramirez@gmail.com>
2024-03-02 21:20:10 +0100

    Fix tests

--- tests/nodejs_nvm.sh
+++ tests/nodejs_nvm.sh
@@ -8,7 +8,7 @@ source commons.sh
 #  None
 #######################################
 function test_main() {
-  run_pengwinsetup install PROGRAMMING NODEJS NVM --debug
+  run_pengwinsetup install PROGRAMMING NODEJS NVM
 
   assertTrue "FILE PROFILE-NVM" "[ -f /etc/profile.d/nvm-prefix.sh ]"
 
