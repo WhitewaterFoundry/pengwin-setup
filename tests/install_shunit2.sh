9c7a31a8c783f77694afb3ab21e626c00e23c49a
Carlos Ramirez <crramirez@gmail.com>
2024-10-23 23:30:58 +0200

    Fix shunit2 checkout directory in install script
    Previously, the script was attempting to checkout a branch without changing to the shunit2 directory. This fix ensures that it navigates into the shunit2 directory before performing the checkout, ensuring the correct execution of the script.

--- tests/install_shunit2.sh
+++ tests/install_shunit2.sh
@@ -4,7 +4,7 @@
   # shellcheck disable=SC2164
   cd
   git clone https://github.com/AJIOB/shunit2.git
-  git checkout AJIOB/add-time-stamps
+  cd shunit2 && git checkout AJIOB/add-time-stamps
   echo "PATH=\"\${PATH}:\${HOME}/shunit2\"" >> "${HOME}"/.bashrc
 )
 
