a64c64b97f02e0cd2b209e5ffc1fa8e00203abdf
Carlos Ramirez <crramirez@gmail.com>
2026-02-05 14:53:13 +0100

    Simplify run function by removing redundant WSL2 environment variable preservation

--- tests/commons.sh
+++ tests/commons.sh
@@ -116,7 +116,7 @@ function run_pengwinsetup() {
 #  None
 #######################################
 function run() {
-  sudo --preserve-env=WSL2 su - -c "env WSL2=${WSL2} $*" "${TEST_USER}" 2>/dev/null
+  sudo --preserve-env=WSL2 su - -c "$*" "${TEST_USER}" 2>/dev/null
 }
 
 #######################################
